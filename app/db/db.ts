b0cd7d15e6eb82b3518d9e761fd91dc37ed4ba58
Ahmed Mansour <133324agh@gmail.com>
2023-11-27 22:50:11 +0200

    Update db.ts

--- app/db/db.ts
+++ app/db/db.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCBbBFWzA2iANKedPBhFvIyFD5ZkNpYVK4",
-  authDomain: "sharek-ada21.firebaseapp.com",
-  projectId: "sharek-ada21",
-  storageBucket: "sharek-ada21.appspot.com",
-  messagingSenderId: "82606865347",
-  appId: "1:82606865347:web:952d66dc8673e50c075b76",
-  measurementId: "G-DJN5QDL283",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_CUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
